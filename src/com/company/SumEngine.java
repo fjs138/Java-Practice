c462f2b86d6e9bfbc54ebf96572c1c3b9da9b9c6
Frank Santaguida <fjs138@gmail.com>
2019-02-15 18:10:37 -0500

    insert accurate and informative description

---
+++ src/com/company/SumEngine.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SumEngine {
+    int sum(int i1, int i2)
+    {int result = 0;
+    for (int i = i1; i <= i2; i++)
+    result +=i;
+    return result;}
+}
---
+++ src/com/company/SumTasks.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SumTasks {
+System.out.println("Sum from 1 to 10 is " + SumEngine(1, 10));
+System.out.println("Sum from 20 to 37 is " + SumEngine(20, 37));
+System.out.println("Sum from 35 to 49 is " + SumEngine(35, 49));
+}
